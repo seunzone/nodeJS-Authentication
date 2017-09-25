21f6c351eaf785bda7b47ef3faeb8d0f4087e2f2
seunzone <darealseun@gmail.com>
2017-09-25 10:48:12 +0100

    Finishing up

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodeJS-Authentication
 A boiler Plate thingy to quickly play with how nodeJS authentication works
+
+This is a very simple and Basic NodeJS Aunthentication built using passportJS and MongoDB as the database.
--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost/auth_demo");
 
 
 //middleWare
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(require('express-session')({
     secret: "I really love dodo",
@@ -22,19 +23,62 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+//=============ROUTES=================
 
 app.get('/', (req,res)=>{
     res.render('home');
 });
 
-app.get('/locked', (req,res)=>{
+app.get('/locked', isLoggedIn, (req,res)=>{
     res.render('locked');
 });
 
+//===========AuthROutes===========
+app.get('/register', (req,res)=>{
+    res.render('register');
+});
+
+app.post('/register', (req,res)=>{
+    // res.send('Post Route');
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, (err,user)=>{
+        if(err){
+            console.log(err)
+            res.render('register');
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/locked");
+        });
+    });
+});
+
+//===============Login Routes=========
+app.get('/login', (req,res)=>{
+    res.render('login');
+});
+
+app.post('/login',passport.authenticate('local', {
+    successRedirect:'/locked',
+    failureRedirect:'login'
+}),(req,res)=>{
+});
+
+//==========Logout Route=========
+app.get('/logout', (req,res)=>{
+    req.logout();
+    res.redirect('/');
+});
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(port, ()=> {
     console.log('Express server is up on port ' + port);
